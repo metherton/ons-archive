f701bdd386dd0f653fa6cd5005f82c0f8779e046
Martin Etherton <metherton@ebay.com>
2012-07-06 22:49:47 +0200

    commiting all changes

--- src/main/scala/gedcom/GedcomParser.scala
+++ src/main/scala/gedcom/GedcomParser.scala
@@ -5,7 +5,9 @@ import io.Source
 class GedcomParser(_gedComFile: String) {
 
   def individuals(): List[GedcomIndividual] = {
-    Source.fromFile(_gedComFile, "latin1").getLines().foreach(line => println(line.length + " " + line))
+    Source.fromFile(_gedComFile, "latin1").getLines().foreach(line => {
+        println(line.length + " " + line)
+    })
     List(new GedcomIndividual("Martin Etherton"))
   }
 
