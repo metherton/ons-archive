4c7320de9233d6743a6b8c4e6e352318a97cbecb
Martin Etherton <metherton@ebay.com>
2012-07-20 10:32:49 +0200

    some renamings

--- src/main/scala/gedcom/GedcomParser.scala
+++ src/main/scala/gedcom/GedcomParser.scala
@@ -6,9 +6,9 @@ import collection.generic.CanBuildFrom
 class GedcomParser(_gedComFile: String) {
 
   def individuals(): List[GedcomIndividual] = {
-    val elements =  parseElements(Source.fromFile(_gedComFile, "latin1").getLines().toList)( _.charAt(0).equals('0') )
-    val mergedElements = mergeLists(Nil, elements).reverse
-    val gedcomRecords = mergedElements map (xs =>
+    val listOf0ElementsAndTheirAssociatedChildElements =  splitFileIntoChunksTokenizedOnLinesBeginningWith0(Source.fromFile(_gedComFile, "latin1").getLines().toList)( _.charAt(0).equals('0') )
+    val listOfLinesGroupedByZeroElement = merge0ElementWithItsChildElements(Nil, listOf0ElementsAndTheirAssociatedChildElements).reverse
+    val gedcomRecords = listOfLinesGroupedByZeroElement map (xs =>
       xs.head.toString match {
         case s if s.startsWith("0 TRLR") => new GedcomTrailer
         case s if s.startsWith("0 @P") => new GedcomIndividual( firstName(xs.tail find (_ startsWith("1 NAME"))),
@@ -83,17 +83,17 @@ class GedcomParser(_gedComFile: String) {
     }
   }
 
-  def mergeLists[A](accum: List[List[A]], xs: List[List[A]]): List[List[A]] = {
+  def merge0ElementWithItsChildElements[A](accum: List[List[A]], xs: List[List[A]]): List[List[A]] = {
     val splitUpList = xs splitAt 2
     splitUpList._2 match {
       case Nil => splitUpList._1.flatten :: accum
-      case _ =>  mergeLists(splitUpList._1.flatten :: accum, splitUpList._2)
+      case _ =>  merge0ElementWithItsChildElements(splitUpList._1.flatten :: accum, splitUpList._2)
     }
   }
 
-  def parseElements[A](xs: List[A])(p: A => Boolean): List[List[A]] =
+  def splitFileIntoChunksTokenizedOnLinesBeginningWith0[A](xs: List[A])(p: A => Boolean): List[List[A]] =
     if (xs.isEmpty) Nil
-    else xs span p match { case (a,b) => a :: parseElements(b)(x => !p(x))}
+    else xs span p match { case (a,b) => a :: splitFileIntoChunksTokenizedOnLinesBeginningWith0(b)(x => !p(x))}
 
 }
 
